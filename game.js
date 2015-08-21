2e30d093a4b09b4f64ce0d389993e4e8941ae7ec
tommulmul2 <tommulmul2@hotmail.com>
2015-08-21 16:41:04 +0200

    Fix and Cleanup + comments
    Cleaned up code and fixed click popup (for real this time) for  Firefox
    and IE, also: comments.

--- game.js
+++ game.js
@@ -8,32 +8,48 @@ var clickValue = 1;
 
 
 
-setInterval(cycle, 500);
+setInterval(cycle, 500); //updates the counter once every 0.5s
 
-function unlockUpgrade() {
-	document.getElementById("upgrade").style.display = "block";
-	document.getElementById("upgradeCost").style.display = "block";
+function makeClickpopUp() { //makes the click popup
+	var clickPopUp = document.createElement("div");
+	clickPopUp.innerHTML = "+" + clickValue;
+	clickPopUp.setAttribute("class", "clickPopUp unselectable default")
+	clickPopUp.style.left = cursorX + "px";
+	clickPopUp.style.top = cursorY + "px";
+
+	var body = document.getElementById("mainDiv");
+	body.insertBefore(clickPopUp, body.lastChild);
+	setTimeout(function() {body.removeChild(clickPopUp)}, 2000);
 }
 
 
 
-function cycle() {
+document.onmousemove = function(e) { //gets mouse POS as CursorX and Y
+	cursorX = e.pageX;
+	cursorY = e.pageY;
+}
+
+
+
+function cycle() { //updates the counter
 	Points += PPS*0.5;
 	update();
 }
 
-function upgrade() {
+function upgrade() { //updates: PPS counter (unhides), upgrade cost, counter
 	document.getElementById("PPS").style.display = "block";
 	document.getElementById("PPS").innerHTML = "PPS: " + Math.round(PPS);
 	document.getElementById("upgradeCost").innerHTML = Math.round(upgradeCost);
+	document.getElementById("counter").innerHTML = Math.round(Points);
 }
 
 
-function update() {
+function update() { //unhides the upgradebutton/upgradecost/counter
 	if (unlockedUpgrade == false) {
 		if (Points >= 100) {
 			unlockedUpgrade = true;
-			unlockUpgrade();
+			document.getElementById("upgrade").style.display = "block";
+			document.getElementById("upgradeCost").style.display = "block";
 		}
 	}
 
@@ -42,35 +58,18 @@ function update() {
 	}
 }
 
-function buttonClick() {
+function buttonClick() { //handles clicking on the button
 
 	if (firstClick == true) {
 		firstClick = false
 	}
 
 
-	Points += 1;
+	Points += clickValue;
 	update();
-
-	var clickPopUp = document.createElement("div");
-	var clickPopUpWorth = document.createTextNode("+" + clickValue);
-	var clickPopUpAtt = document.createAttribute("class");
-	var body = document.getElementById("mainDiv");
-	clickPopUpAtt.value = "clickPopUp unselectable default"
-	clickPopUp.appendChild(clickPopUpWorth);
-	clickPopUp.setAttributeNode(clickPopUpAtt);
-	var mouseX = event.clientX;
-	var mouseY = event.clientY;
-	clickPopUp.style.left = mouseX + "px";
-	clickPopUp.style.top = mouseY + "px";
-
-	
-
-	body.appendChild(clickPopUp);
-	setTimeout(function() {body.removeChild(clickPopUp)}, 2000);
 }
 
-function upgradeClick() {
+function upgradeClick() { //handles purchasing an upgrade
 	if (Points >= upgradeCost) {
 		Points -= upgradeCost
 		upgradeCost *= 1.1
